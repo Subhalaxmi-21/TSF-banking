5deab82bac1625b9246f3c447f62a0d2ac740941
Subhalaxmi Das <subhalaxmi.das@somaiya.edu>
2021-12-19 15:02:24 +0530

    change1

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ app.use('/api/transaction', require('./routes/Transaction'))
   const PORT = process.env.PORT || 3001
   if (process.env.NODE_ENV == "production"){
     app.use(express.static("banking/build"));
-    // app.get('*', (req, res) => {
-    //   res.sendFile(path.resolve(__dirname, 'banking', 'build', 'index.html'))
-    // })
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'banking', 'build', 'index.html'))
+    })
   }
 
   app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
